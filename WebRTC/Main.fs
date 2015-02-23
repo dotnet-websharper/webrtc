2765de3c1f67f8af5e718fd984ff9db73616ebb5
Istvan Gansperger <gansperger@gmail.com>
2015-02-23 19:33:22 +0100

    Fixed namespace

--- WebRTC/Main.fs
+++ WebRTC/Main.fs
@@ -245,7 +245,7 @@ module Definition =
 
     let Assembly = 
         Assembly [
-            Namespace "IntelliFactory.WebSharper.JavaScript" [
+            Namespace "WebSharper.JavaScript" [
                 MediaStreamTrackState
                 SourceTypeEnum
                 Constraints.DoubleRange
