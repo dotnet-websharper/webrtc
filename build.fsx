fca36d1debee949d7aa447283339ef5415bc8183
Loic Denuziere <loic@denuziere.net>
2018-01-24 13:30:47 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,35 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.WebRTC")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    bt.WebSharper.Extension("IntelliFactory.WebSharper.WebRTC", directory = "WebRTC")
-        .SourcesFromProject("WebRTC.fsproj")
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-(*let test =
-    bt.WebSharper.BundleWebsite("IntelliFactory.WebSharper.WebRTC.Tests", directory = "Tests")
-        .SourcesFromProject("Tests.fsproj")
-        .References(fun r -> [r.Project main])*)
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-bt.Solution [
-    main
-    //test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.WebRTC"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.webrtc"
-                Description = "WebSharper Extensions for WebRTC"
-                Authors = ["IntelliFactory"]
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
